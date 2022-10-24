d8c9c6145fa4793550aa2ad414d6c92463a42955
Shriniwas Sharma <shriniwas26@gmail.com>
2022-10-24 19:08:23 +0200

    Cleanup data download script

--- update_data.py
+++ update_data.py
@@ -1,32 +1,22 @@
 #!/usr/bin/env python3
-import requests
+import json
 import os
-import io
+import datetime
+
 import pandas as pd
 
 DATA_URL = "https://data.rivm.nl/covid-19/COVID-19_aantallen_gemeente_cumulatief.csv"
+SELF_FILE_PATH = os.path.realpath(__file__)
+SELF_FILE_DIR = os.path.split(SELF_FILE_PATH)[0]
 
-self_file_path = os.path.realpath(__file__)
-self_file_dir = os.path.split(self_file_path)[0]
-output_filepath = os.path.join(
-    self_file_dir,
-    "data/COVID-19_aantallen_gemeente_cumulatief.csv"
-)
 
 def update():
-    req = requests.get(DATA_URL)
-    assert req.status_code == 200
-    df = pd.read_csv(io.StringIO(req.text), sep=";")
-    print(f"Successful request to {DATA_URL}")
-    temp_file = output_filepath + ".tmp"
-    df.to_csv(
-        temp_file,
-        sep=";",
-        index=False,
-        line_terminator="\r\n"
+    output_filepath = os.path.join(
+        SELF_FILE_DIR, "data", "COVID-19_aantallen_gemeente_cumulatief.csv"
     )
-    os.replace(temp_file, output_filepath)
-
+    df = pd.read_csv(DATA_URL, sep=";")
+    print(f"Successful request to {DATA_URL}")
+    df.to_csv(output_filepath, sep=";", index=False, lineterminator="\n")
 
 if __name__ == "__main__":
     print("Downloading file...")
