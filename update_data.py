9cfda033ba77235b7462206a0d99706b50819744
Shriniwas Sharma <shriniwas@maaind.com>
2024-06-18 14:44:18 +0200

    Data update script

--- update_data.py
+++ update_data.py
@@ -1,17 +1,18 @@
 #!/usr/bin/env python3
-import requests
-import os
 import io
+import os
+
 import pandas as pd
+import requests
 
 DATA_URL = "https://data.rivm.nl/covid-19/COVID-19_aantallen_gemeente_cumulatief.csv"
 
 file_path = os.path.realpath(__file__)
 file_dir = os.path.split(file_path)[0]
 output_filepath = os.path.join(
-    file_dir,
-    "data/COVID-19_aantallen_gemeente_cumulatief.csv"
+    file_dir, "data/COVID-19_aantallen_gemeente_cumulatief.csv"
 )
+
 
 def update():
     req = requests.get(DATA_URL)
@@ -23,7 +24,6 @@ def update():
         temp_file,
         sep=";",
         index=False,
-        line_terminator="\r\n"
     )
     os.replace(temp_file, output_filepath)
 
