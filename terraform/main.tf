b82b6ddc38a7cbde7169d651c3d042aa56688d1e
Shriniwas Sharma <shriniwas_sharma@pm.me>
2025-07-05 22:17:28 +0200

    HTTP working

--- terraform/main.tf
+++ terraform/main.tf
@@ -244,44 +244,18 @@ resource "aws_lb_target_group" "app" {
   }
 }
 
-# SSL Certificate (for HTTPS)
-resource "aws_acm_certificate" "main" {
-  domain_name       = "covid-dashboard-alb-352395096.eu-north-1.elb.amazonaws.com"
-  validation_method = "DNS"
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
 resource "aws_lb_listener" "http" {
   load_balancer_arn = aws_lb.main.arn
   port              = "80"
   protocol          = "HTTP"
 
   default_action {
-    type = "redirect"
-
-    redirect {
-      port        = "443"
-      protocol    = "HTTPS"
-      status_code = "HTTP_301"
-    }
-  }
-}
-
-resource "aws_lb_listener" "https" {
-  load_balancer_arn = aws_lb.main.arn
-  port              = "443"
-  protocol          = "HTTPS"
-  ssl_policy        = "ELBSecurityPolicy-2016-08"
-  certificate_arn   = aws_acm_certificate.main.arn
-
-  default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.app.arn
   }
 }
+
+
 
 # ECS Service
 resource "aws_ecs_service" "app" {
