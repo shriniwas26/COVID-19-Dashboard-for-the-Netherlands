6cb5da0ccade85b4f73231955521f9225f8c6c0a
Shriniwas Sharma <shriniwas_sharma@pm.me>
2025-07-13 19:37:00 +0200

    Reduce desired_count to 1 (for costs)

--- terraform/main.tf
+++ terraform/main.tf
@@ -353,7 +353,7 @@ resource "aws_ecs_service" "app" {
   name            = "covid-dashboard-service"
   cluster         = aws_ecs_cluster.main.id
   task_definition = aws_ecs_task_definition.app.arn
-  desired_count   = 2
+  desired_count   = 1
   launch_type     = "FARGATE"
 
   network_configuration {
